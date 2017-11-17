8d01fee9708809114b4f486e0069c69c03de4826
leosu <leosu@shunshunliuxue.com>
2017-11-17 16:34:02 +0800

    @import variable.scss

--- src/components/Recommend/Recommend.vue
+++ src/components/Recommend/Recommend.vue
@@ -44,11 +44,14 @@ export default {
   }
 }
 </script>
-<style>
+<style lang="scss">
+
+@import '../../assets/css/variable.scss';
 
 li {
   display: block;
   margin: 20px 0;
+  font-size: $font-size-medium;
 }
 </style>
 
